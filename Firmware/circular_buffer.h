18e15174fca8196c3eaff6c4d2442557b4637ac0
andrew <andrewnaughton71@gmail.com>
2023-02-07 21:44:40 -0500

    got struct working with strings

--- Firmware/circular_buffer.h
+++ Firmware/circular_buffer.h
@@ -6,24 +6,28 @@
 #define JADE_3D_PRINTER_CIRCULAR_BUFFER_H
 
 #include <limits.h>
+#include <stdio.h>
+#include <string.h>
 
 #define BUFFER_SIZE 10
-#define EMPTY_INDEX_VAL INT_MAX
-
+#define STR_SIZE 100
 
 typedef struct
 {
     int head; // where elements can be added to the buffer
     int curr; // current element of the buffer
-    int buff[BUFFER_SIZE];
+    int length;
+    char element[BUFFER_SIZE][STR_SIZE];
 }circular_buffer;
 
 void init_buffer(circular_buffer *buffer){
     buffer->head = 0;
     buffer->curr = 0;
-    for(int i = 0; i < BUFFER_SIZE; i++){
-        buffer->buff[i] = EMPTY_INDEX_VAL;
-    }
+    buffer->length = strlen(buffer->element);
+}
+
+char empty_index_value(char element[]){
+    return *strcpy(element, NULL);
 }
 
 int is_empty(circular_buffer *buffer){
@@ -34,22 +38,23 @@ int is_full(circular_buffer *buffer){
     return (buffer->head + 1) % BUFFER_SIZE == buffer->curr;
 }
 
-void enqueue(circular_buffer *buffer, int item){
+void enqueue(circular_buffer *buffer, char *item){
     if(is_full(buffer)){
         exit(1);
     }
-    buffer->buff[buffer->head] = item;
+    strcpy(buffer->element[buffer->curr], item);
     buffer->head = (buffer->head + 1) % BUFFER_SIZE;
+    buffer->length++;
 }
 
-int dequeue(circular_buffer *buffer){
+char* dequeue(circular_buffer *buffer){
+    static char removed_item[STR_SIZE];
     if(is_empty(buffer)){
-        exit(1);
+        return NULL;
     }
-    int item = buffer->buff[buffer->curr];
-    buffer->buff[buffer->curr] = EMPTY_INDEX_VAL;
+    strcpy(removed_item, buffer->element[buffer->curr]);
     buffer->curr = (buffer->curr + 1) % BUFFER_SIZE;
-    return item;
+    return removed_item;
 }
 
 #endif //JADE_3D_PRINTER_CIRCULAR_BUFFER_H
--- Firmware/main.c
+++ Firmware/main.c
@@ -5,14 +5,20 @@
 int main(void){
     circular_buffer cb;
     init_buffer(&cb);
+    char txt[] = "abcde";
+    enqueue(&cb, txt);
+    // enqueue(&cb, 1121);
+    // enqueue(&cb, 23498756);
+    char *removed = dequeue(&cb);
+    char *a = &removed[0];
+    printf("Dequeued: %s\n", removed);
+    printf("first element: %c\n", removed[0]);
+    printf("second element: %c\n", removed[1]);
+    printf("third element: %c\n", removed[2]);
+    printf("fourth element: %c\n", removed[3]);
 
-    enqueue(&cb, 1000);
-    enqueue(&cb, 1121);
-    enqueue(&cb, 23498756);
-
-    printf("Dequeued: %d\n", dequeue(&cb));
-    printf("Dequeued: %d\n", dequeue(&cb));
-    printf("Dequeued: %d\n", dequeue(&cb));
+    // printf("Dequeued: %d\n", dequeue(&cb));
+    // printf("Dequeued: %d\n", dequeue(&cb));
 
     return 0;
 }
